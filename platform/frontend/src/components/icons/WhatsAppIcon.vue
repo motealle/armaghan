57d8b94d7370b954a069711bdf21232c60ff44d3
motealle <motealle@gmail.com>
2026-09-20 03:09:34 +0330

    Add reusable WhatsApp icon component

---
+++ platform/frontend/src/components/icons/WhatsAppIcon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ size?: number; decorative?: boolean }>(), { size: 22, decorative: true })
+</script>
+
+<template>
+  <img
+    src="./icons/whatsapp.svg"
+    class="whatsapp-brand-icon"
+    :width="size"
+    :height="size"
+    :alt="decorative ? '' : 'WhatsApp'"
+    :aria-hidden="decorative ? 'true' : undefined"
+  />
+</template>
