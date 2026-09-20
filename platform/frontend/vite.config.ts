d6ae6913bfd227b74a8cf9ea8f75983e06911b20
motealle <motealle@gmail.com>
2026-09-20 05:21:11 +0330

    Build Vue app into Test 16

--- platform/frontend/vite.config.ts
+++ platform/frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
   },
   build: {
-    outDir: '../../t/15',
+    outDir: '../../t/16',
     emptyOutDir: true,
     sourcemap: true,
     target: 'es2022',
